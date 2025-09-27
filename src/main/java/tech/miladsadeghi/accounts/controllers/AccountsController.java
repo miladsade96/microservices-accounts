7a382bca0c32e1cfafb59425e92b6ba6ccc52ffd
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-27 15:01:41 +0330

    Add sayHello endpoint to AccountsController

--- src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
+++ src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
@@ -1,4 +1,12 @@
 package tech.miladsadeghi.accounts.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountsController {
+    @GetMapping("sayHello")
+    public String sayHello() {
+        return "Hello from Accounts Service!";
+    }
 }
