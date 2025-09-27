d407da52eb6d17844a2fc2097a678a626fc13000
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-27 21:57:23 +0330

    Add createAccount endpoint to AccountsController for account creation

--- src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
+++ src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
@@ -1,12 +1,25 @@
 package tech.miladsadeghi.accounts.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.miladsadeghi.accounts.constants.AccountsConstants;
+import tech.miladsadeghi.accounts.dtos.CustomerDTO;
+import tech.miladsadeghi.accounts.dtos.ResponseDTO;
 
 @RestController
+@RequestMapping(path = "/api/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountsController {
-    @GetMapping("sayHello")
-    public String sayHello() {
-        return "Hello from Accounts Service!";
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
+
 }
