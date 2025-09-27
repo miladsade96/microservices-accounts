a071ddfa3d9a4b844e120742193fc190d9b6aa61
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-27 20:45:10 +0330

    Implementing Accounts database entity class

--- src/main/java/tech/miladsadeghi/accounts/entities/Accounts.java
+++ src/main/java/tech/miladsadeghi/accounts/entities/Accounts.java
@@ -1,4 +1,23 @@
 package tech.miladsadeghi.accounts.entities;
 
-public class Accounts {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Accounts extends BaseEntity {
+
+    private Long customerId;
+
+    @Id
+    private Long accountNumber;
+
+    private String accountType;
+    private String branchAddress;
 }
