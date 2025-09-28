af9acde9f0d5ad7823c9089e15b43641e34eccef
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 09:47:51 +0330

    Refactor AccountsController to use IAccountsService for account creation

--- src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
+++ src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
@@ -1,5 +1,6 @@
 package tech.miladsadeghi.accounts.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 import tech.miladsadeghi.accounts.constants.AccountsConstants;
 import tech.miladsadeghi.accounts.dtos.CustomerDTO;
 import tech.miladsadeghi.accounts.dtos.ResponseDTO;
+import tech.miladsadeghi.accounts.services.IAccountsService;
 
 @RestController
 @RequestMapping(path = "/api/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
+
+    private final IAccountsService iAccountsService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
+        iAccountsService.createAccount(customerDTO);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
