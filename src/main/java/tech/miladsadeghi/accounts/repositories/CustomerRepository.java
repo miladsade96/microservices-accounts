ac6f5de05665407694821962bfa20b93d3beab7d
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 09:44:43 +0330

    Add findByMobileNumber method to CustomerRepository for mobile number lookup

--- src/main/java/tech/miladsadeghi/accounts/repositories/CustomerRepository.java
+++ src/main/java/tech/miladsadeghi/accounts/repositories/CustomerRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.miladsadeghi.accounts.entities.Customer;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
