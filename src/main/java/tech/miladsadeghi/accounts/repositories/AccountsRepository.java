70be7e5389fc35fa3dfadd58693ca90f7e844fda
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 13:19:55 +0330

    Adding deleteByCustomerId method

--- src/main/java/tech/miladsadeghi/accounts/repositories/AccountsRepository.java
+++ src/main/java/tech/miladsadeghi/accounts/repositories/AccountsRepository.java
@@ -1,6 +1,8 @@
 package tech.miladsadeghi.accounts.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import tech.miladsadeghi.accounts.entities.Accounts;
 
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
