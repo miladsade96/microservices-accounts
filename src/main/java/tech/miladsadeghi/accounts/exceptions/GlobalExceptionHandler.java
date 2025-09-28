e70841ab2e6aa7b2f1ed5e23deccd31222edb483
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 09:47:38 +0330

    Add GlobalExceptionHandler to manage CustomerAlreadyExistsException

---
+++ src/main/java/tech/miladsadeghi/accounts/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package tech.miladsadeghi.accounts.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import tech.miladsadeghi.accounts.dtos.ErrorResponseDTO;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCustomerAlreadyExistsException(
+            CustomerAlreadyExistsException ex, WebRequest request) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                request.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
