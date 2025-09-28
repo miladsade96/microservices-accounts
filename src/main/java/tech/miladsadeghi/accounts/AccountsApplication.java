b8936b0dc7413da400056bfae868b5daacb1c6e7
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 15:11:24 +0330

    Adding info to swagger docs

--- src/main/java/tech/miladsadeghi/accounts/AccountsApplication.java
+++ src/main/java/tech/miladsadeghi/accounts/AccountsApplication.java
@@ -1,11 +1,31 @@
 package tech.miladsadeghi.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts MicroService RestAPI Documentation",
+                version = "v1.0.0",
+                contact = @Contact(
+                        name = "Milad Sadeghi",
+                        email = "miladsadeghidm@gmail.com",
+                        url = "https://miladsadeghi.tech"
+                ),
+                description = "This is a sample Accounts MicroService RESTful API documentation generated using OpenAPI 3.",
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
