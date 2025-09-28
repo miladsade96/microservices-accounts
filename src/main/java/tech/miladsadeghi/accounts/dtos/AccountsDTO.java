c22de00c8d919630665e1a4e8c13263fe7ac1ac7
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-09-28 14:07:01 +0330

    Adding validation annotations to dto classes

--- src/main/java/tech/miladsadeghi/accounts/dtos/AccountsDTO.java
+++ src/main/java/tech/miladsadeghi/accounts/dtos/AccountsDTO.java
@@ -1,11 +1,19 @@
 package tech.miladsadeghi.accounts.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDTO {
 
+    @NotEmpty(message = "Account number must not be empty")
+    @Pattern(regexp = "^\\d{10}$", message = "Account number must be a 10-digit numeric value")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type must not be empty")
     private String accountType;
+
+    @NotEmpty(message = "Branch address must not be empty")
     private String branchAddress;
 }
--- src/main/java/tech/miladsadeghi/accounts/dtos/CustomerDTO.java
+++ src/main/java/tech/miladsadeghi/accounts/dtos/CustomerDTO.java
@@ -1,12 +1,24 @@
 package tech.miladsadeghi.accounts.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
 
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Email must not be empty")
+    @Email(regexp = "^[\\w.-]+@[\\w.-]+\\.\\w{2,}$", message = "Email must be a valid email address")
     private String email;
+
+    @Pattern(regexp = "^(\\+98|0)?9\\d{9}$", message = "Mobile number must be a valid mobile number")
     private String mobileNumber;
+
     private AccountsDTO accountsDTO;
 }
