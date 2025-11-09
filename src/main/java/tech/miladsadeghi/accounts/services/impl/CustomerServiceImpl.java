e1f58a19f2142be596503ba38a4d898025ef2a0d
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-09 20:17:56 +0330

    Implement CustomerServiceImpl to fetch customer details including accounts, loans, and cards

---
+++ src/main/java/tech/miladsadeghi/accounts/services/impl/CustomerServiceImpl.java
@@ -0,0 +1,49 @@
+package tech.miladsadeghi.accounts.services.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import tech.miladsadeghi.accounts.dtos.AccountsDTO;
+import tech.miladsadeghi.accounts.dtos.CardsDTO;
+import tech.miladsadeghi.accounts.dtos.CustomerDetailsDTO;
+import tech.miladsadeghi.accounts.dtos.LoansDTO;
+import tech.miladsadeghi.accounts.entities.Accounts;
+import tech.miladsadeghi.accounts.entities.Customer;
+import tech.miladsadeghi.accounts.exceptions.ResourceNotFoundException;
+import tech.miladsadeghi.accounts.mappers.AccountsMapper;
+import tech.miladsadeghi.accounts.mappers.CustomerMapper;
+import tech.miladsadeghi.accounts.repositories.AccountsRepository;
+import tech.miladsadeghi.accounts.repositories.CustomerRepository;
+import tech.miladsadeghi.accounts.services.ICustomerService;
+import tech.miladsadeghi.accounts.services.client.CardsFeignClient;
+import tech.miladsadeghi.accounts.services.client.LoansFeignClient;
+
+@Service
+@AllArgsConstructor
+public class CustomerServiceImpl implements ICustomerService {
+
+    private final AccountsRepository accountsRepository;
+    private final CustomerRepository customerRepository;
+    private final CardsFeignClient cardsFeignClient;
+    private final LoansFeignClient loansFeignClient;
+
+    @Override
+    public CustomerDetailsDTO fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+
+        Accounts account = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", String.valueOf(customer.getCustomerId())));
+
+        CustomerDetailsDTO customerDetailsDTO = CustomerMapper.mapToCustomerDetailsDTO(customer, new CustomerDetailsDTO());
+        customerDetailsDTO.setAccountsDTO(AccountsMapper.mapToAccountsDTO(account, new AccountsDTO()));
+
+        ResponseEntity<LoansDTO> loansDTOResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDTO.setLoansDTO(loansDTOResponseEntity.getBody());
+
+        ResponseEntity<CardsDTO> cardsDTOResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDTO.setCardsDTO(cardsDTOResponseEntity.getBody());
+
+        return customerDetailsDTO;
+    }
+}
