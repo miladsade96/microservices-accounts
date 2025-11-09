71352d4e07074e64938bbe4d41f2f4e6ecef93f2
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-09 20:17:47 +0330

    Add ICustomerService interface with fetchCustomerDetails method

---
+++ src/main/java/tech/miladsadeghi/accounts/services/ICustomerService.java
@@ -0,0 +1,8 @@
+package tech.miladsadeghi.accounts.services;
+
+import tech.miladsadeghi.accounts.dtos.CustomerDetailsDTO;
+
+public interface ICustomerService {
+
+    CustomerDetailsDTO fetchCustomerDetails(String mobileNumber);
+}
