2aec3e241f79c3aa4965c779cfd9d0c10acf10f7
Milad Sadeghi <miladsadeghidm@gmail.com>
2025-12-06 21:04:15 +0330

    Adding rate limiter to getJavaVersion endpoint in AccountsController

--- src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
+++ src/main/java/tech/miladsadeghi/accounts/controllers/AccountsController.java
@@ -1,5 +1,6 @@
 package tech.miladsadeghi.accounts.controllers;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -210,6 +211,7 @@ public class AccountsController {
             }
     )
     @GetMapping("/java-version")
+    @RateLimiter(name = "getJavaVersion")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
     }
